459a644ad5165248ee3201fa83819b25bb3547b2
RyanDu <77805079+JialeDu@users.noreply.github.com>
2025-09-26 23:48:18 -0400

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://${S3BUCKET}/SpringBootHelloWorldExampleApplication.war \
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-xmobce67pspx/SpringBootHelloWorldExampleApplication.war \
   /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
